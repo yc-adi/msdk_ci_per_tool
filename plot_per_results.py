e633157ee7f9bf2c52be95578a37665ded2eb2cc
Ying Cai <ying.cai@analog.com>
2023-02-10 10:18:32 -0600

    Adaptive to single column case.

--- plot_per_results.py
+++ plot_per_results.py
@@ -72,11 +72,11 @@ parser.add_argument('basename', help='PDF file base name.')
 
 args = parser.parse_args()
 
-print("csvFile  :", args.csvFile)
+print("csvFile :", args.csvFile)
 
 csv_full_path = f'{args.csvFile}'
 csv_full_path = os.path.expanduser(csv_full_path)
-print(f'csv full:"{csv_full_path}')
+print(f'csv full: {csv_full_path}')
 
 pdf_file_name = args.csvFile.replace('.csv', '.pdf')
 pdf_file_name = os.path.expanduser(pdf_file_name)
@@ -106,11 +106,21 @@ print(f'row: {row}, col: {col}')
 if row > 1 or col > 1:
     fig, axs = plt.subplots(row, col)
 
+    name = pdf_file_name.split('/')[-1]
+    board = name.split('_')[2].replace('.pdf', '').upper()
+    fig.suptitle(f'Packet Error Rate vs Attenuation\n{board}', fontsize=10)
+
+    if axs.ndim == 1:
+        plt.subplots_adjust(top=0.83, hspace=0.5)
+    else:
+        fig.tight_layout()
+        plt.subplots_adjust(top=0.80, bottom=0.1, hspace=0.5)
+
     case = 0
     for packetLen, phy, txPower in itertools.product(lens, phys, txPowers):
         print(f'CASE: {case + 1}')
-        col = int(phy - 1)
-        row = int((case + 1 - phy) / len(phys))
+        col = case % len(phys)
+        row = int(case / len(phys))
         print(f'row: {row}, col: {col}')
 
         # Create line plot with atten to perSlave
@@ -119,34 +129,42 @@ if row > 1 or col > 1:
         print("txPower :", txPower)
         tempDf = df.loc[(df['packetLen'] == packetLen) & (
             df['phy'] == phy) & (df['txPower'] == txPower)]
-        
+        '''
         name = pdf_file_name.split('/')[-1]
         board = name.split('_')[2].replace('.pdf', '').upper()
         fig.suptitle(f'Packet Error Rate vs Attenuation\n{board}', fontsize=10)
         fig.tight_layout()
         plt.subplots_adjust(bottom=0.1)
-
-        axs[row, col].set_title(f'packet len: {packetLen}\nphy: {phy_str[phy]}\ntxPower:{txPower}',
-                                fontdict={'fontsize': 6, 'fontweight': 'medium'})
-        axs[row, col].set_xlabel('Attenuation, dBm', fontdict={"fontsize": 5})
-        axs[row, col].set_ylabel('PER (%)', fontdict={"fontsize": 5})
-
-        axs[row, col].tick_params(axis='both', which='major', labelsize=4)
-
-        axs[row, col].plot(tempDf["atten"], tempDf["perSlave"],
-                           "-x", linewidth=0.25, ms=0.5)
-
-        axs[row, col].axhline(y=SPEC, color='r', linestyle=':', linewidth=0.5)
+        '''
+        title = f'packet len: {packetLen}\nphy: {phy_str[phy]}\ntxPower:{txPower}'
+        if axs.ndim == 1:
+            axs[row].set_title(title, fontdict={'fontsize': 6, 'fontweight': 'medium'})
+            axs[row].set_xlabel('Attenuation, dBm', fontdict={"fontsize": 5})
+            axs[row].set_ylabel('PER (%)', fontdict={"fontsize": 5})
+            axs[row].tick_params(axis='both', which='major', labelsize=4)
+            axs[row].plot(tempDf["atten"], tempDf["perSlave"], "-x", linewidth=0.25, ms=0.5)
+            axs[row].axhline(y=SPEC, color='r', linestyle=':', linewidth=0.5)
+        else:
+            axs[row, col].set_title(title, fontdict={'fontsize': 6, 'fontweight': 'medium'})
+            axs[row, col].set_xlabel('Attenuation, dBm', fontdict={"fontsize": 5})
+            axs[row, col].set_ylabel('PER (%)', fontdict={"fontsize": 5})
+            axs[row, col].tick_params(axis='both', which='major', labelsize=4)
+            axs[row, col].plot(tempDf["atten"], tempDf["perSlave"], "-x", linewidth=0.25, ms=0.5)
+            axs[row, col].axhline(y=SPEC, color='r', linestyle=':', linewidth=0.5)
 
         a = list(tempDf['atten'])
         p = list(tempDf['perSlave'])
         for i in range(len(a)):
             if p[i] > SPEC:
                 print(f'{a[i]}, {p[i]}')
-                axs[row, col].axvline(
-                    x=a[i], color='r', linestyle=':', linewidth=0.5)
-                axs[row, col].text(a[i], p[i], f'  {p[i]}% @ {a[i]} dBm', horizontalalignment='left',
-                                   verticalalignment='center', fontsize=3)
+                if axs.ndim == 1:
+                    axs[row].axvline(x=a[i], color='r', linestyle=':', linewidth=0.5)
+                    axs[row].text(a[i], p[i], f'  {p[i]}% @ {a[i]} dBm', horizontalalignment='left',
+                                  verticalalignment='center', fontsize=3)
+                else:
+                    axs[row, col].axvline(x=a[i], color='r', linestyle=':', linewidth=0.5)
+                    axs[row, col].text(a[i], p[i], f'  {p[i]}% @ {a[i]} dBm', horizontalalignment='left',
+                                       verticalalignment='center', fontsize=3)
                 break
 
         # note
@@ -209,7 +227,7 @@ for packetLen, phy, txPower in itertools.product(lens, phys, txPowers):
 
     # resize the figure to match the aspect ratio of the Axes
     fig.set_size_inches(7, 8, forward=True)
-    fig.text(.5, .10, args.desc, ha='center', fontdict={"fontsize": 12})
+    #fig.text(.5, .10, args.desc, ha='center', fontdict={"fontsize": 12})
     fig.text(.5, .05, "Run on all data channels (no advertising channels).", ha='center',
              fontdict={"fontsize": 12})
 
